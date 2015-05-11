67219936b705965a345eefaeedbdbf7c5ff7aef4
Klemen Brodej <klemen.brodej1@gmail.com>
2015-05-11 09:11:09 +0200

    Revert "ds"
    This reverts commit ca9f485bae388e4d42ab885cdb98f334320f1d48.

---
+++ footer.php
@@ -0,0 +1,9 @@
+  
+<div>
+<footer>
+      <p><img src="images/twitter.png" alt="twitter" />&nbsp;<img src="images/facebook.png" alt="facebook" />&nbsp;<img src="images/rss.png" alt="rss" /></p>
+      <p><a href="index.html">Home</a> | <a href="examples.html">Examples</a> | <a href="page.html">A Page</a> | <a href="another_page.html">Another Page</a> | <a href="contact.php">Contact Us</a></p>
+      <p>Copyright &copy; CSS3_grass | <a href="http://www.css3templates.co.uk">design from css3templates.co.uk</a></p>
+    </footer>
+  </div>
+
